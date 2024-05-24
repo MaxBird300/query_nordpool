00fbcc1e0cfb19b026e11495ed1ca86dc7202c8c
Max Bird <maxbird300@gmail.com>
2024-05-24 16:32:44 +0100

    updated authentication process

--- backend.py
+++ backend.py
@@ -25,7 +25,10 @@ class NordPoolClass:
         self.mySession = requests.Session()
         self.subKey = sub_key
         
-    def authenticateUser(self):
+        self.set_new_authentication_token()
+        
+        
+    def set_new_authentication_token(self):
         """
         Returns authentication token as explained in - https://developers.nordpoolgroup.com/docs/auth-introduction
     
@@ -43,14 +46,18 @@ class NordPoolClass:
             response = self.mySession.post("https://sts.nordpoolgroup.com/connect/token", data = payload, headers = headers)
             if response.status_code == 200:
                 print("Authentication success! Token received.")
-                self.token = response.json()['access_token'] 
+                self.token = response.json()['access_token']
+                self.token_expiration = (
+                    datetime.datetime.now() 
+                    + datetime.timedelta(seconds=response.json()['expires_in'])
+                    )
+
             else:
                 print("Authentication failed, status code: %s" % response.status_code)
-                sys.exit(0)
+
         except requests.exceptions.RequestException as err:
             print(err)
-            sys.exit(0)
-
+            
 
     def apiResponse(self, host, payload, headers):
 
@@ -67,34 +74,92 @@ class NordPoolClass:
             sys.exit(0)
             
     
-    def dayAheadData(self, startDate, endDate):
+    # def dayAheadData(self, startDate, endDate):
         
-        # Authenticate and assign token
-        self.authenticateUser()
+    #     print('This function is deprecated please use another.')
         
-        # change string dates to ISO datetime
-        startTime = strDate2iso(startDate)
-        endTime = strDate2iso(endDate)
+    #     # Authenticate and assign token if required
+    #     if datetime.datetime.now() > self.token_expiration:
+    #         self.set_new_authentication_token()
         
-        # change the below 'host' 'payload' and 'headers' to access different parts of the API found here - https://marketdata.nordpoolgroup.com/docs/services/MarketData-UK-v2/operations/UkAreaBlockPrices 
-        host = "https://marketdata-api.nordpoolgroup.com/uk/v2/n2ex/prices/area?" 
-        payload = {"startTime": startTime,
-                   "endTime": endTime}
-        headers = {"Accept-Encoding": '',
-                    "Ocp-Apim-Subscription-Key": self.subKey,
-                    "Authorization": "Bearer " + self.token}
+    #     # change string dates to ISO datetime
+    #     startTime = strDate2iso(startDate)
+    #     endTime = strDate2iso(endDate)
         
-        # get response object from API
+    #     # change the below 'host' 'payload' and 'headers' to access different parts of the API found here - https://marketdata.nordpoolgroup.com/docs/services/MarketData-UK-v2/operations/UkAreaBlockPrices 
+    #     host = "https://marketdata-api.nordpoolgroup.com/uk/v2/n2ex/prices/area?" 
+    #     payload = {"startTime": startTime,
+    #                "endTime": endTime}
+    #     headers = {"Accept-Encoding": '',
+    #                 "Ocp-Apim-Subscription-Key": self.subKey,
+    #                 "Authorization": "Bearer " + self.token}
+        
+    #     # get response object from API
+    #     response = self.apiResponse(host, payload, headers)
+    #     hourly_prices = response.json()[0]['values'] # dictionary of hourly prices
+        
+    #     df = pd.DataFrame(
+    #         data = {
+    #             'Timestamp': [x['startTime'] for x in hourly_prices],
+    #             'Price (GBP/MWh)': [x['value'] for x in hourly_prices]          
+    #             }
+    #         )
+        
+    #     return df
+    
+    
+    def UKdayAheadPricesV2(
+            self,
+            delivery_date: str # delivery CET date, yyyy-MM-dd format e.g. 2024-01-21
+            ):
+        """
+        Returns settled hourly day-ahead prices for UK for specified delivery_date.
+        
+        Prices are set 9am the day before delivery I think...
+        """
+
+        # Authenticate and assign token if required
+        if datetime.datetime.now() > self.token_expiration:
+            self.set_new_authentication_token()
+        
+        
+        host = 'https://data-api.nordpoolgroup.com/api/v2/Auction/Prices/ByAreas?'
+        payload = {
+            'market': 'N2EX_DayAhead',
+            'areas': 'UK',
+            'currency': 'GBP',
+            'date': delivery_date
+            }
+        headers = {
+            "Accept-Encoding": '',
+            "Ocp-Apim-Subscription-Key": self.subKey,
+            "Authorization": "Bearer " + self.token
+            }
         response = self.apiResponse(host, payload, headers)
-        hourly_prices = response.json()[0]['values'] # dictionary of hourly prices
-        
+        price_dicts = response.json()[0]['prices'] # list of dicts
+
         df = pd.DataFrame(
             data = {
-                'Timestamp': [x['startTime'] for x in hourly_prices],
-                'Price (GBP/MWh)': [x['value'] for x in hourly_prices]          
+                'Timestamp': [x['deliveryStart'] for x in price_dicts],
+                'Price (GBP/MWh)': [x['price'] for x in price_dicts]          
                 }
             )
         
         return df
-
+    
+    # def UkdayAheadPriceCurve(
+    #         self,
+    #         delivery_date: str # delivery CET date, yyyy-MM-dd format e.g. 2024-01-21
+    #         ):
+        
+    #     host = 'https://data-api.nordpoolgroup.com/api/v2/Auction/N2EX_DayAhead/PriceCurves/UK?'
+        
+        
+        
+        
+        
+        
+        
+        
+        
         
--- example.py
+++ example.py
@@ -12,7 +12,9 @@ import config
 startDate = '01/01/2020' # don't query more than one month at a time, dd/mm/yyyy format
 endDate = '01/02/2020'
 
+delivery_date = '2024-05-25' # yyyy-MM-dd format
+
 
 nordPoolAPI = NordPoolClass(config.username, config.password, config.sub_key) # initialise the API connection
-df = nordPoolAPI.dayAheadData(startDate, endDate) # Timestamp in output dataframe relates to the start of the time period
-print(df.head())
+# df = nordPoolAPI.dayAheadData(startDate, endDate) # Timestamp in output dataframe relates to the start of the time period
+df_v2 = nordPoolAPI.UKdayAheadPricesV2(delivery_date)
