bf812755b36d5bbcfbf9fd4685cd97755db46f5c
Max Bird <maxbird300@gmail.com>
2024-05-28 14:07:52 +0100

    simplified example

--- example.py
+++ example.py
@@ -9,13 +9,9 @@ Documentation for API can be found here - https://marketdata.nordpoolgroup.com/d
 from backend import NordPoolClass
 import config
 
-startDate = '01/01/2020' # don't query more than one month at a time, dd/mm/yyyy format
-endDate = '01/02/2020'
-
-delivery_date = '2024-05-25' # yyyy-MM-dd format
+delivery_date = '2012-01-01' # yyyy-MM-dd format
 
 
 nordPoolAPI = NordPoolClass(config.username, config.password, config.sub_key) # initialise the API connection
-# df = nordPoolAPI.dayAheadData(startDate, endDate) # Timestamp in output dataframe relates to the start of the time period
-df_v2 = nordPoolAPI.UKdayAheadPricesV2(delivery_date)
+df = nordPoolAPI.UKdayAheadPricesV2(delivery_date)
 
